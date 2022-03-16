dd80ad4d0e674baecaf4028544b9b0b1296d0739
8Whack <calvinpratt99@gmail.com>
2022-03-16 16:38:37 -0700

    added budgetReducer

---
+++ src/redux/budgetReducer.js
@@ -0,0 +1,11 @@
+var initalState = {
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+  }
+
+function reducer(state = initalState, action) {
+    return state
+}
+
+export default reducer;
